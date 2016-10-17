63710efe314e17a5f5dd156d0ce6e39e99b27099
naarisan <qazaqaza98@gmail.com>
2016-10-18 01:10:00 +0900

    delete some code

--- bot.py
+++ bot.py
@@ -6,54 +6,10 @@ consumer_secret = os.environ['SINE_CONSUMER_SECRET']
 access_token = os.environ['SINE_ACCESS_KEY']
 access_secret = os.environ['SINE_ACCESS_SECRET']
 
-class DotAccessible(object): # http://d.hatena.ne.jp/karasuyamatengu/20120408/1333862237
-    """オブジェクトグラフ内の辞書要素をプロパティ風にアクセスすることを可能にするラッパー。
-        DotAccessible( { 'foo' : 42 } ).foo==42
-
-    メンバーを帰納的にワップすることによりこの挙動を下層オブジェクトにも与える。
-        DotAccessible( { 'lst' : [ { 'foo' : 42 } ] } ).lst[0].foo==42
-    """
-
-    def __init__(self, obj):
-        self.obj=obj
-
-    def __repr__(self):
-        return "DotAccessible(%s)" % repr(self.obj)
-
-    def __getitem__(self, i):
-        """リストメンバーをラップ"""
-        return self.wrap(self.obj[i])
-
-    def __getslice__(self, i, j):
-        """リストメンバーをラップ"""
-
-        return map(self.wrap, self.obj.__getslice__(i,j))
-
-    def __getattr__(self, key):
-        """辞書メンバーをプロパティとしてアクセス可能にする。
-        辞書キーと同じ名のプロパティはアクセス不可になる。
-        """
-
-        if isinstance(self.obj, dict):
-            try:
-                v=self.obj[key]
-            except KeyError:
-                v=self.obj.__getattribute__(key)
-        else:
-            v=self.obj.__getattribute__(key)
-
-        return self.wrap(v)
-
-    def wrap(self, v):
-        """要素をラップするためのヘルパー"""
-
-        if isinstance(v, (dict,list,tuple)): # xx add set
-            return self.__class__(v)
-        return v
-
 class StreamListener(tweepy.StreamListener):
     def __init__(self, api):
         super().__init__()
+        print(dir(self))
         self.api = api
         self.me = self.api.me()
     def on_status(self, status):
@@ -62,7 +18,6 @@ class StreamListener(tweepy.StreamListener):
             print(status.user.name)
             print("--------------------")
             print(status.text)
-
     def on_event(self, event):
         if event.event == 'follow':
             source_user = event.source
@@ -70,12 +25,6 @@ class StreamListener(tweepy.StreamListener):
                 print("followed by {} {}".format(source_user["name"], source_user["id"]))
                 event._api.create_friendship(source_user["id"])
                 print("followed {} {}".format(source_user["name"], source_user["id"]))
-        # if event.event == 'unfollow':
-        #     target_user = event.target
-        #     print("unfollowed by {} {}".format(target_user["name"], target_user["id"]))
-        #     event._api.destroy_friendship(target_user["id"])
-        #     print("unfollowed {} {}".format(target_user["name"], target_user["id"]))
-
     def on_error(self, status_code):
         if status_code == 420:
             print(str(status_code))
