479c7f5dd5510901f37c0d2cbf456940fe7d756b
naarisan <qazaqaza98@gmail.com>
2016-10-20 02:19:09 +0900

    change tweet crawl from streaming API to REST API

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+import os, time, datetime, threading
 import tweepy
 
 from logging import getLogger,StreamHandler,DEBUG
@@ -14,26 +14,18 @@ access_token = os.environ['SINE_ACCESS_KEY']
 access_secret = os.environ['SINE_ACCESS_SECRET']
 
 omaega_sine = "@{} お前が死ね"
-search = "死ね"
+query = "死ね"
+ng_words = ["RT", "死ねない"]
+ng_names = ["bot", "BOT", "Bot"]
 
-class StreamListener(tweepy.StreamListener):
+class FollowStreamListener(tweepy.StreamListener):
     def __init__(self, api):
-        super().__init__()
-        self.api = api
+        super().__init__(api)
         self.me = self.api.me()
-    def on_status(self, status):
-        if not status.retweeted:
-            if search in status.text:
-                # print(status)
-                logger.info("sine")    
-                print("============================================================")
-                print(status.user.name, "@"+status.user.screen_name)
-                print("------------------------------------------------------------")
-                print(status.text)
-                # status._api.update_status(omaega_sine.format(status.user.screen_name), in_reply_to_status_id=status.id)
     def on_event(self, event):
         if event.event == 'follow':
             source_user = event.source
+            print("aa")
             if self.me.id != source_user["id"]:
                 print("============================================================")
                 print("followed by {} {}".format(source_user["name"], source_user["id"]))
@@ -44,17 +36,59 @@ class StreamListener(tweepy.StreamListener):
             print(str(status_code))
             return False
 
+class TweetCrawler(threading.Thread):
+    def __init__(self, api, ng_names, ng_words, latest_tweet_id=None, cooltime=10):
+        super().__init__(daemon=True)
+        self.api = api
+        self.ng_names = ng_names
+        self.ng_words = ng_words
+        self.latest_tweet_id = latest_tweet_id
+    def tweet_filter(self, tweet):
+        if tweet.retweeted:
+            return False
+        for ngn, ngw in zip(self.ng_names, self.ng_words):
+            if ngn in tweet.user.name or ngw in tweet.text:
+                return False
+        else:
+            return True
+    def tweet_crawl(self):
+        logger.info("crawling")
+        for i, tweet in enumerate(self.api.search(q=query, count=100)):
+            if self.latest_tweet_id == None:
+                self.latest_tweet_id = tweet.id
+            if i == 0:
+                new_latest_tweet_id = tweet.id
+            if tweet.id > self.latest_tweet_id:
+                if self.tweet_filter(tweet):
+                    logger.info("{} {} {} {}\n{}\n".format(tweet.created_at, tweet.id, tweet.user.name, tweet.user.screen_name, tweet.text))
+            else:
+                break
+        self.latest_tweet_id = new_latest_tweet_id
+    def run(self):
+        while True:
+            self.tweet_crawl()
+            time.sleep(10)
+
+class TweetUpdater(threading.Thread):
+    def run(self):
+        while True:
+            pass
+
 if __name__ == "__main__":
-    print("hello")
     logger.info("hello")
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
 
-    u_stream = tweepy.Stream(auth=api.auth, listener=StreamListener(api))
-    f_stream = tweepy.Stream(auth=api.auth, listener=StreamListener(api))
+    u_stream = tweepy.Stream(auth=api.auth, listener=FollowStreamListener(api))
 
-    u_stream.userstream(track=search)
-    # f_stream.filter(track=search, async=True)
-    print("yeah")
+    crawler = TweetCrawler(api, ng_names, ng_words)
+    crawler.start()
+
+    while True:
+        try:
+            u_stream.userstream(track=query)
+        except:
+            u_stream = tweepy.Stream(auth=api.auth, listener=FollowStreamListener(api))
+
     logger.info("yeah")
