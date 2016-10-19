61f4c4fa6a229311c9ffdedce576f20b6982f890
naarisan <qazaqaza98@gmail.com>
2016-10-20 02:51:07 +0900

    add TweetUpdater

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os, time, datetime, threading
 import tweepy
+from queue import Queue
 
 from logging import getLogger,StreamHandler,DEBUG
 logger = getLogger(__name__)
@@ -37,11 +38,13 @@ class FollowStreamListener(tweepy.StreamListener):
             return False
 
 class TweetCrawler(threading.Thread):
-    def __init__(self, api, ng_names, ng_words, latest_tweet_id=None, cooltime=10):
+    def __init__(self, api, query, ng_names, ng_words, tweet_queue, latest_tweet_id=None, cooltime=10):
         super().__init__(daemon=True)
         self.api = api
+        self.query = query
         self.ng_names = ng_names
         self.ng_words = ng_words
+        self.tweet_queue = tweet_queue
         self.latest_tweet_id = latest_tweet_id
     def tweet_filter(self, tweet):
         if tweet.retweeted:
@@ -53,13 +56,14 @@ class TweetCrawler(threading.Thread):
             return True
     def tweet_crawl(self):
         logger.info("crawling")
-        for i, tweet in enumerate(self.api.search(q=query, count=100)):
+        for i, tweet in enumerate(self.api.search(q=self.query, count=100)):
             if self.latest_tweet_id == None:
                 self.latest_tweet_id = tweet.id
             if i == 0:
                 new_latest_tweet_id = tweet.id
             if tweet.id > self.latest_tweet_id:
                 if self.tweet_filter(tweet):
+                    tweet_queue.put(tweet)
                     logger.info("{} {} {} {}\n{}\n".format(tweet.created_at, tweet.id, tweet.user.name, tweet.user.screen_name, tweet.text))
             else:
                 break
@@ -70,9 +74,20 @@ class TweetCrawler(threading.Thread):
             time.sleep(10)
 
 class TweetUpdater(threading.Thread):
+    def __init__(self, tweet_queue, omaega_sine, cooltime=60):
+        super().__init__(daemon=True)
+        self.tweet_queue = tweet_queue
+        self.latest_tweeted_time = time.time()
+        self.omaega_sine = omaega_sine
+        self.cooltime = cooltime
     def run(self):
         while True:
-            pass
+            if not self.tweet_queue.empty():
+                tweet = tweet_queue.get()
+                remaining_time = time.time() - self.latest_tweeted_time
+                if not remaining_time > self.cooltime:
+                    time.sleep(remaining_time)
+                tweet._api().update_status(self.omaega_sine.format(tweet.user.screen_name), in_reply_to_status_id=tweet.id)
 
 if __name__ == "__main__":
     logger.info("hello")
@@ -82,8 +97,12 @@ if __name__ == "__main__":
 
     u_stream = tweepy.Stream(auth=api.auth, listener=FollowStreamListener(api))
 
-    crawler = TweetCrawler(api, ng_names, ng_words)
+    tweet_queue = Queue()
+
+    crawler = TweetCrawler(api, query, ng_names, ng_words, tweet_queue)
+    tweeter = TweetUpdater(tweet_queue, omaega_sine)
     crawler.start()
+    tweeter.start()
 
     while True:
         try:
